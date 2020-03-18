afe43cc899bdb5cd609d5cbdc1d347509028bb0c
Maximilian Mitchell <max@maxis.me>
2020-03-18 00:06:42 +0000

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PROJECT="grafananotifi"
+
+. ~/.profile
+
+cd $(dirname "$0")
+
+# pull latest from project you have created
+git fetch origin
+git checkout master
+if git merge $1; then
+
+    # build binary
+    go build -o /usr/local/bin/$PROJECT .
+
+    # reload binary
+    systemctl restart $PROJECT.service
+else
+    echo "problem merging"
+    exit 1
+fi
