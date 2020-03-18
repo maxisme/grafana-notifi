84293c37dd0bdf4940af85b3ed64e921c3ea45a4
Maximilian Mitchell <max@maxis.me>
2020-03-18 00:35:01 +0000

    remove file logs

--- main.go
+++ main.go
@@ -4,23 +4,11 @@ import (
 	"github.com/coreos/go-systemd/activation"
 	"github.com/go-chi/chi"
 	"github.com/tylerb/graceful"
-	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
-var f *os.File
 func main() {
-	var err error
-	f, err = os.OpenFile("/var/log/grafana-notifi.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-
-	log.SetOutput(f)
-
 	m := chi.NewRouter()
 	m.HandleFunc("/api", ApiProxyHandler)
 
